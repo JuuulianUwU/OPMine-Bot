028ef8ea7e9168a4a2787e25fa12a814b71bd090
JuuulianUwU <70039325+JuuulianUwU@users.noreply.github.com>
2020-09-13 22:41:29 +0200

    First

---
+++ main.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js'); 
+const bot = new Discord.Client();
+const prefix = '.'; 
+
+bot.on('ready', () => {
+    console.log('OPMine-Bot ist jetzt Online!'); 
+})
+
+
+bot.on('message', message =>{
+    if (message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    if (command === 'team') {
+        const testEmbed = new Discord.MessageEmbed()
+           .setImage('https://i.imgur.com/ZJOtS6f.png') 
+           .setTitle('⮩   Unser Server-Team   ⮨')
+           .setColor(0x45B8AC)
+           .setFooter('⮞ Developed & Designed by _7u ⮜')
+
+        try {
+            message.channel.send(testEmbed);
+        } catch {
+            message.reply(`Sorry <@${message.autor.username}> Ich kann momentan nicht helfen :c`)
+        }
+    }
+ 
+    
+})
+
+bot.login(process.env.token); 
